64afa77ac884af59a86d4d3cf69734d10b7b1d68
alekgjer <aleksandergj@gmail.com>
2019-02-06 13:15:21 +0100

    Aleks var her

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
 print("Project in modelling and optimization of heat flow")
-print("Project in modelling and optimization of heat flow")
-
-
-class Environment:
-    def __init__(self):
-        self.length = 10  # Meter
-        self.width = 10  # Meter
-    def boundaries(self):
-        
+print("Aleks var her")
