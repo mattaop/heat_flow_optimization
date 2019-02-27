3ac2244617fa09410e63b3e4013aba598a901f8a
alekgjer <aleksandergj@gmail.com>
2019-02-27 15:18:29 +0100

    Minor tweaks

--- heat_eqn_2d.py
+++ heat_eqn_2d.py
@@ -59,4 +59,4 @@ placement = (5, 5)
 
 square_room = grid_modell_2d(x_len, y_len, Nx, Ny, initial_temperature, heater_temperature, temperature_outside, placement)
 #print(np.shape(square_room.temperature_matrix))
-square_room.find_temperature_after_n_timesteps(100000)
+square_room.find_temperature_after_n_timesteps(10000000)
