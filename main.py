6701225df2216c7014129fcbacddc3bef4c4e8df
mattaop <snowboard95@live.no>
2019-03-06 12:44:14 +0100

    main module added

--- main.py
+++ main.py
@@ -19,16 +19,14 @@ def load_initial_values():
 
 def start_optimization():
     initial_values = load_initial_values()
-    print(initial_values["temperature_outside"])
 
+    heater_placement = (0, 0)
     square_room = HE.GridModel2D()
     optimizing_algorithm = BO.BayesianOptimization()
-
     # Run the simulation for two random values to get samples for the optimization algorithm
     for i in range(2):
         time = square_room.simulate()
         optimizing_algorithm.update_samples(square_room.heater_placement, time)
-
     # Run until we get a optimal solution
     optimizing = True
     while optimizing:
