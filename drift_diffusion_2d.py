1ea4fb44da55dd614a1f5319e574e3db1044cece
alekgjer <aleksandergj@gmail.com>
2019-03-27 17:05:44 +0100

    Fix convection-diffusion and can be used for optimization. Also a new set of IC to be used in json file

--- drift_diffusion_2d.py
+++ drift_diffusion_2d.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-
+import json
 
 class GridModel2D_DD:
     length = 0
@@ -19,7 +19,7 @@ class GridModel2D_DD:
     temperature_matrix = []
     temperature_matrix_previous_time = []
     time = 0
-    max_time = 1000000
+    max_time = 10000000
     v_x = []
     v_y = []
     a_x = []
@@ -44,21 +44,21 @@ class GridModel2D_DD:
         # Propagate with forward-difference in time, central-difference in space
         self.temperature_matrix[1:-1, 1:-1] = self.temperature_matrix_previous_time[1:-1, 1:-1] + self.thermal_diffusivity * self.dt * ((self.temperature_matrix_previous_time[2:, 1:-1] - 2 * self.temperature_matrix_previous_time[1:-1, 1:-1] + self.temperature_matrix_previous_time[:-2, 1:-1]) / self.dx ** 2 + (self.temperature_matrix_previous_time[1:-1, 2:] - 2 * self.temperature_matrix_previous_time[1:-1, 1:-1] + self.temperature_matrix_previous_time[1:-1, :-2]) / self.dy ** 2)
         self.temperature_matrix[1:-1, 1:-1] -= (self.temperature_matrix_previous_time[2:, 1:-1] - self.temperature_matrix_previous_time[:-2, 1:-1])*self.dt/(2*self.dx) * self.v_x[1:-1, 1:-1]
-        self.temperature_matrix[1:-1, 1:-1] -= (self.temperature_matrix_previous_time[1:-1,2:] - self.temperature_matrix_previous_time[1:-1,:-2])*self.dt/(2*self.dy) * self.v_y[1:-1, 1:-1]
+        self.temperature_matrix[1:-1, 1:-1] -= (self.temperature_matrix_previous_time[1:-1, 2:] - self.temperature_matrix_previous_time[1:-1, :-2])*self.dt/(2*self.dy) * self.v_y[1:-1, 1:-1]
         self.temperature_matrix[1:-1, 1:-1] -= self.dt*(self.temperature_matrix_previous_time[1:-1, 1:-1]*self.a_x[1:-1, 1:-1] + self.temperature_matrix_previous_time[1:-1, 1:-1]*self.a_y[1:-1, 1:-1])
         self.temperature_matrix[0, :] = (9 * self.temperature_matrix_previous_time[1, :] + self.temperature_outside) / 10
         self.temperature_matrix[-1, :] = (9 * self.temperature_matrix_previous_time[-2, :] + self.temperature_outside) / 10
         self.temperature_matrix[:, 0] = (9 * self.temperature_matrix_previous_time[:, 1] + self.temperature_outside) / 10
         self.temperature_matrix[:, -1] = (9 * self.temperature_matrix_previous_time[:, -2] + self.temperature_outside) / 10
         self.temperature_matrix[self.heater_placement] = self.heater_temperature
+        self.temperature_matrix[self.temperature_matrix > self.heater_temperature] = self.heater_temperature
         self.temperature_matrix_previous_time = self.temperature_matrix
         self.time += self.dt
 
     def plot_temperature_room(self):
         print("avg_temp in room: ", np.mean(self.temperature_matrix))
         print("Time: ", self.time)
-        plt.imshow(self.temperature_matrix, cmap=plt.get_cmap('hot'), vmin=self.initial_temperature,
-                   vmax=self.heater_temperature)
+        plt.imshow(self.temperature_matrix, cmap=plt.get_cmap('hot'), vmin=min(self.initial_temperature, self.temperature_outside), vmax=self.heater_temperature)
         plt.colorbar()
         plt.show()
 
@@ -70,7 +70,17 @@ class GridModel2D_DD:
 
         self.time = 0
         self.v_x, self.v_y = self.create_velocity_field()
+        self.v_x /= 1000
+        self.v_y /= 1000
+        # self.v_x = np.ones((self.Nx, self.Ny))
+        # self.v_y = np.ones((self.Nx, self.Ny))
+        # self.v_x /= 1000
+        # self.v_y /= 1000
+        #print(np.max(self.v_x), np.min(self.v_x))
         self.a_x, self.a_y = self.create_change_in_velocity_field()
+        self.a_x /= 1000
+        self.a_y /= 1000
+        #print(np.max(self.a_x), np.min(self.a_x))
         self.temperature_matrix_previous_time = np.ones((self.Nx, self.Ny))*self.initial_temperature
         self.temperature_matrix_previous_time[self.heater_placement] = self.heater_temperature
         self.temperature_matrix = np.zeros_like(self.temperature_matrix_previous_time)
@@ -83,7 +93,7 @@ class GridModel2D_DD:
         v_x = np.zeros((self.Nx, self.Ny))
         v_y = np.zeros_like(v_x)
         x_H, y_H = self.heater_placement[0], self.heater_placement[1]
-        print(x_H, y_H)
+        #print(x_H, y_H)
         for i in range(self.Nx):
             for j in range(self.Ny):
                 if (i, j) != (x_H, y_H):
@@ -96,7 +106,7 @@ class GridModel2D_DD:
         a_x = np.zeros((self.Nx, self.Ny))
         a_y = np.zeros_like(a_x)
         x_H, y_H = self.heater_placement[0], self.heater_placement[1]
-        print(x_H, y_H)
+        #print(x_H, y_H)
         for i in range(self.Nx):
             for j in range(self.Ny):
                 if (i, j) != (x_H, y_H):
@@ -107,19 +117,9 @@ class GridModel2D_DD:
 
 
 # Eksempel på bruk av kode
-# temperature_outside = 20+273
-# initial_temperature, heater_temperature = 15+273, 30+273
-# x_len, y_len, Nx, Ny = 5, 5, 10, 10
-# placement = (5, 5)
-#
-# number_timesteps = 1000
-#
-# square_room = GridModel2D(x_len, y_len, Nx, Ny, initial_temperature, heater_temperature, temperature_outside, placement)
-# square_room.find_temperature_after_n_timesteps(number_timesteps)
-#
-# square_room.heater_placement = (1, 1)
-# square_room.time = 0
-# square_room.temperature_matrix_previous_time = np.ones((square_room.Nx, square_room.Ny))*square_room.initial_temperature
-# square_room.temperature_matrix_previous_time[square_room.heater_placement] = square_room.heater_temperature
-# square_room.find_temperature_after_n_timesteps(number_timesteps-10)
-
+# parameters = json.load(open("initial_values/aleksander.json", "r"))
+# heater_placement = [15, 15]
+# square_room = GridModel2D_DD(parameters)
+# square_room.simulate(heater_placement)
+# print(square_room.temperature_matrix)
+# square_room.plot_temperature_room()
