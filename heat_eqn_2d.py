9fdf69dfa80072a7be8f219449da12b68943599c
alekgjer <aleksandergj@gmail.com>
2019-04-02 16:39:18 +0200

    Comment code and fix small bugs. Both ready for simulation

--- heat_eqn_2d.py
+++ heat_eqn_2d.py
@@ -1,27 +1,35 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import json
 
 
 class GridModel2D:
+    """
+    Class containing a rectangle room and can be used to simulate the 2D heat equation given an IC and BC
+    """
     length = 0
     width = 0
-    Nx = 0
-    Ny = 0
-    dx, dy = 0, 0
-    thermal_diffusivity = 19*10**(-6)
-    initial_temperature = 0
-    temperature_outside = 0
-    heater_temperature = 0
-    heater_placement = 0
-    temperature_goal = 0
-    dt = 0
+    Nx = 0  # number of grid points in the x-direction
+    Ny = 0  # number of grid points in the y-direction
+    dx, dy = 0, 0  # spacing between grid points on the x- and y-axis
+    thermal_diffusivity = 19*10**(-6)  # constant value
+    initial_temperature = 0  # temperature of the room
+    temperature_outside = 0  # temperature outside
+    heater_temperature = 0  # temperature of the heater
+    heater_placement = 0  # the placement of the heater in the grid
+    temperature_goal = 0  # the wanted average temperature to end the simulation
+    dt = 0  # time step value
     temperature_matrix = []
     temperature_matrix_previous_time = []
-    time = 0
-    max_time = 10000000
+    time = 0  # value to keep track of time used in the simulation
+    max_time = 10000000  # max limit of time allowed for the simulation
 
     def __init__(self, parameters):
+        """
+        Setting initial values for the simulation of the 2d heat equation from json file
+        :param parameters: Dict from json file which includes values to be used in the simulation
+        """
         self.length = parameters['simulation']['xlen']
         self.width = parameters['simulation']['ylen']
         self.Nx = parameters['simulation']['Nx']
@@ -37,53 +45,59 @@ class GridModel2D:
         self.temperature_matrix = np.zeros_like(self.temperature_matrix_previous_time)
 
     def _temperature_at_new_timestep_ftcs(self):
-        # Propagate with forward-difference in time, central-difference in space
+        """
+        Function to compute the temperature at each grid point at a new time step using the FTCS scheme for 2d heat equation
+        """
         self.temperature_matrix[1:-1, 1:-1] = self.temperature_matrix_previous_time[1:-1, 1:-1] + self.thermal_diffusivity * self.dt * ((self.temperature_matrix_previous_time[2:, 1:-1] - 2 * self.temperature_matrix_previous_time[1:-1, 1:-1] + self.temperature_matrix_previous_time[:-2, 1:-1]) / self.dx ** 2 + (self.temperature_matrix_previous_time[1:-1, 2:] - 2 * self.temperature_matrix_previous_time[1:-1, 1:-1] + self.temperature_matrix_previous_time[1:-1, :-2]) / self.dy ** 2)
-        self.temperature_matrix[0, :] = (9 * self.temperature_matrix_previous_time[1, :] + self.temperature_outside) / 10
-        self.temperature_matrix[-1, :] = (9 * self.temperature_matrix_previous_time[-2, :] + self.temperature_outside) / 10
-        self.temperature_matrix[:, 0] = (9 * self.temperature_matrix_previous_time[:, 1] + self.temperature_outside) / 10
-        self.temperature_matrix[:, -1] = (9 * self.temperature_matrix_previous_time[:, -2] + self.temperature_outside) / 10
-        self.temperature_matrix[self.heater_placement] = self.heater_temperature
+        self.temperature_matrix[0, :] = (9 * self.temperature_matrix_previous_time[1, :] + self.temperature_outside) / 10  # wall update
+        self.temperature_matrix[-1, :] = (9 * self.temperature_matrix_previous_time[-2, :] + self.temperature_outside) / 10  # wall update
+        self.temperature_matrix[:, 0] = (9 * self.temperature_matrix_previous_time[:, 1] + self.temperature_outside) / 10  # wall update
+        self.temperature_matrix[:, -1] = (9 * self.temperature_matrix_previous_time[:, -2] + self.temperature_outside) / 10  # wall update
+        self.temperature_matrix[self.heater_placement] = self.heater_temperature  # reset the temperature of the heater
         self.temperature_matrix_previous_time = self.temperature_matrix
-        self.time += self.dt
+        self.time += self.dt  # increment in time for each time step
 
     def plot_temperature_room(self):
+        """
+        Function to plot the temperature in the room using the values contained in temperature_matrix
+        """
         print("avg_temp in room: ", np.mean(self.temperature_matrix))
         print("Time: ", self.time)
-        plt.imshow(self.temperature_matrix, cmap=plt.get_cmap('hot'), vmin=self.initial_temperature, vmax=self.heater_temperature)
+        plt.imshow(self.temperature_matrix, cmap=plt.get_cmap('hot'),
+                   vmin=min(self.initial_temperature, self.temperature_outside), vmax=self.heater_temperature)
         plt.colorbar()
+        plt.xlabel('y')
+        plt.ylabel('x')
+        plt.title('Simulated temperature from heater placement: {}'.format(self.heater_placement))
         plt.show()
 
     def simulate(self, heater_placement='Random'):
+        """
+        For a given heater_placement, uses FTCS to update the temperature at each time step until stopping criterion
+        :param heater_placement: [x,y] array with the heater position, if not provided a random position is used
+        :return: time used in the simulation
+        """
         if heater_placement == 'Random':
             self.heater_placement = (random.randint(0, self.Nx-1), random.randint(0, self.Ny-1))
         else:
             self.heater_placement = (heater_placement[0], heater_placement[1])
 
+        # reinitialize time and temperature matrices used in the simulation to be able to use the object multiple times
         self.time = 0
         self.temperature_matrix_previous_time = np.ones((self.Nx, self.Ny))*self.initial_temperature
         self.temperature_matrix_previous_time[self.heater_placement] = self.heater_temperature
         self.temperature_matrix = np.zeros_like(self.temperature_matrix_previous_time)
+        # until the average temperature of the room is above the temperature goal take a new time step
+        # also make use of max_time in the cases where the room never achieve the goal temperature
         while np.mean(self.temperature_matrix) < self.temperature_goal and self.time < self.max_time:
             self._temperature_at_new_timestep_ftcs()
-        print(np.mean(self.temperature_matrix), self.time, self.heater_placement)
+        print(self.time)
         return self.time
 
 
-# Eksempel på bruk av kode
-# temperature_outside = 20+273
-# initial_temperature, heater_temperature = 15+273, 30+273
-# x_len, y_len, Nx, Ny = 5, 5, 10, 10
-# placement = (5, 5)
-#
-# number_timesteps = 1000
-#
-# square_room = GridModel2D(x_len, y_len, Nx, Ny, initial_temperature, heater_temperature, temperature_outside, placement)
-# square_room.find_temperature_after_n_timesteps(number_timesteps)
-#
-# square_room.heater_placement = (1, 1)
-# square_room.time = 0
-# square_room.temperature_matrix_previous_time = np.ones((square_room.Nx, square_room.Ny))*square_room.initial_temperature
-# square_room.temperature_matrix_previous_time[square_room.heater_placement] = square_room.heater_temperature
-# square_room.find_temperature_after_n_timesteps(number_timesteps-10)
-
+# Example code which can be used to simulate the temperature in a room for a given json file and heater placement
+# parametersAleks = json.load(open("initial_values/aleksander.json", "r"))
+# heater_place = [5, 5]
+# square_room = GridModel2D(parametersAleks)
+# square_room.simulate(heater_place)
+# square_room.plot_temperature_room()
