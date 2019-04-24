3e61f8a68471eb5719faf126ca793fdb5ef04911
mattaop <snowboard95@live.no>
2019-04-24 11:06:26 +0200

    Times the algorithms

--- main.py
+++ main.py
@@ -34,34 +34,49 @@ def start_optimization():
     square_room = DD.GridModel2D_DD(parameters)
     optimizing_algorithm = BO.BayesianOptimization(parameters)
     test_function = test.TestFunction()
+    optimization_time = 0
+    simulation_time = 0
 
     # Run the simulation for two random values to get samples for the optimization algorithm
     for i in range(10):
         print('Simulate location', len(optimizing_algorithm.t_samples)+1, '...')
-        time = square_room.simulate()
-        optimizing_algorithm.update_samples(square_room.heater_placement, scale_time(time))
-    square_room.plot_temperature_room()
+        start_time = time.time()
+        time_at_placement = square_room.simulate()
+        simulation_time += time.time()-start_time
+        
+        start_time = time.time()
+        optimizing_algorithm.update_samples(square_room.heater_placement, scale_time(time_at_placement))
+        optimization_time += time.time()-start_time
+    # square_room.plot_temperature_room()
 
     # Run until we get convergence
+    start_time = time.time()
     while True:
         heater_placement = optimizing_algorithm.propose_location()
         print("Expected improvement left: ", optimizing_algorithm.ei)
         print("Threshold for convergence: ", optimizing_algorithm.threshold)
         if optimizing_algorithm.check_convergence():
             break
+        optimization_time += time.time()-start_time
+        start_time = time.time()
         print('Simulate location', len(optimizing_algorithm.t_samples)+1, '...')
-        time = square_room.simulate(heater_placement)
-        optimizing_algorithm.update_samples(square_room.heater_placement, scale_time(time))
-    return len(optimizing_algorithm.t_samples+1)
+        time_at_placement = square_room.simulate(heater_placement)
+        simulation_time += time.time()-start_time
+        start_time = time.time()
+        optimizing_algorithm.update_samples(square_room.heater_placement, scale_time(time_at_placement))
+    return len(optimizing_algorithm.t_samples+1), optimization_time, simulation_time
 
 
 if __name__ == '__main__':
-    number_of_trials = 10
+    number_of_trials = 1
     number_of_iterations = np.zeros([number_of_trials])
     time_per_iteration = np.zeros([number_of_trials])
+    optimization_time = np.zeros([number_of_trials])
+    simulation_time = np.zeros([number_of_trials])
     for i in range(number_of_trials):
         start_time = time.time()
-        number_of_iterations[i] = start_optimization()
+        number_of_iterations[i], optimization_time[i], simulation_time[i] = start_optimization()
         time_per_iteration[i] = time.time()-start_time
     print('Average number of iterations over ', number_of_trials, ' trials: ', number_of_iterations.mean())
-    print('Average time over ', number_of_trials, 'trials: ', time_per_iteration.mean())
+    print('Average time over ', number_of_trials, 'trials: ', time_per_iteration.mean(), 's , with optimization: ',
+          optimization_time.mean(), ' s, and simulation: ', simulation_time.mean(), ' s.')
